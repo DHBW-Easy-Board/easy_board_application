b01ebbc742717a251bbff5fa950e55ae12a00b21
Jonny Vang <jonny-vang@web.de>
2023-04-05 20:21:43 +0200

    EB-112 added default navbar component

--- src/app/shared/components/container/container.component.ts
+++ src/app/shared/components/container/container.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    NavBarComponent
+  ],
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.scss']
 })
