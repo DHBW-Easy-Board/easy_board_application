ab687c8e0a21858cf99abe65905dfda62c9e1094
Jonny Vang <jonny-vang@web.de>
2023-04-05 19:32:48 +0200

    EB-112 added app container * overall container which can hold nav bars, footer, ...

--- src/app/features/kanban/kanban.module.ts
+++ src/app/features/kanban/kanban.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { KanbanRoutingModule } from './kanban-routing.module';
 import { MatButtonModule } from '@angular/material/button';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AppContainerComponent } from 'src/app/shared/components/app-container/app-container.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
     CommonModule,
     MatButtonModule,
     KanbanRoutingModule,
+    AppContainerComponent
   ]
 })
 export class KanbanModule { }
---
+++ src/app/shared/components/app-container/app-container.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-app-container',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app-container.component.html',
+  styleUrls: ['./app-container.component.scss']
+})
+export class AppContainerComponent {
+
+}
