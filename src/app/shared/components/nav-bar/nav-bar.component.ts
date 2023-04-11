112919e8edb0dd1e3363265c61a9f5ba219ca545
Jonny Vang <jonny-vang@web.de>
2023-04-11 23:03:57 +0200

    EB-112 added initial board shortcut to navbar

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { supabase } from 'src/env/supabase';
+import { Board } from 'src/app/core/models/board.model';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
@@ -10,13 +14,40 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   imports: [
     CommonModule,
     MatButtonModule,
+    MatDividerModule,
     MatIconModule,
+    MatMenuModule,
     MatToolbarModule,
   ],
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
+    public latestBoards: Board[] = [];
+
     // Customization
     public logoUrl: string = 'assets/img/logo.png';
+
+    ngOnInit() {
+        this.getLatestBoards();
+    }
+
+    private async getLatestBoards() {
+        await supabase.auth.getUser()
+            .then((response) => {
+                response.data.user?.id
+
+                supabase.from('board_ov_vw')
+                    .select('*')
+                    .eq('owner_id', response.data.user?.id)
+                    .limit(3)
+                    .order('board_modify_ts', { ascending: false })
+                    .then((response) => {
+                        if (response.error)
+                            return;
+
+                        this.latestBoards = response.data as Board[];
+                    });
+            });
+    }
 }
