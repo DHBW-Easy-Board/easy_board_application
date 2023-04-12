50242f868570708408970c2b21601029c18bdfce
Jonny Vang <jonny-vang@web.de>
2023-04-12 22:24:56 +0200

    EB-49 added slide overlay

--- src/app/shared/components/container/container.component.ts
+++ src/app/shared/components/container/container.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { SlideComponent } from '../slide/slide.component';
 
 @Component({
   selector: 'app-container',
   standalone: true,
   imports: [
     CommonModule,
-    NavBarComponent
+    NavBarComponent,
+    SlideComponent,
   ],
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.scss']
