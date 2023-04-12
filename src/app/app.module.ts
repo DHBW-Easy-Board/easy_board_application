13626f53e3784d43c58cfecdb351cb4aafa7d67b
CamelliaEnjoyer <BGAGpEi9rdxWrMd7BSuc>
2023-04-12 16:33:29 +0200

    EB-51 idk why no working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateCardComponent } from './features/kanban/components/create-card/create-card.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatInputModule} from "@angular/material/input";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatNativeDateModule} from "@angular/material/core";
 
 @NgModule({
   declarations: [
     AppComponent,
+    CreateCardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/features/kanban/components/create-card/create-card.component.ts
@@ -0,0 +1,59 @@
+import {Component, Input, ViewChild} from '@angular/core';
+import {FormBuilder} from "@angular/forms";
+import {CdkTextareaAutosize} from "@angular/cdk/text-field";
+import {supabase} from "../../../../../env/supabase";
+
+export interface assignee{
+  id: string
+}
+
+@Component({
+  selector: 'app-create-card',
+  templateUrl: './create-card.component.html',
+  styleUrls: ['./create-card.component.scss']
+})
+export class CreateCardComponent {
+
+  @Input()
+  public boardId: number = 0;
+
+  @Input()
+  public columnId: number = 0;
+
+  userId: any = 0;
+  userMail: any = "isitdefault";
+
+  asignees: string[] = ["123"];
+
+  constructor(private formBuilder:FormBuilder) { }
+
+  @ViewChild('autosize') autosize: CdkTextareaAutosize | undefined;
+
+  addCardForm = this.formBuilder.group({
+      fName:'',
+      fAssignee:'',
+      fDueDate:'',
+      fDescription: ''
+    }
+  );
+
+  public async submitCreateCardForm(){
+    await supabase.from('card').insert([
+      {name: this.addCardForm.value.fName, description: this.addCardForm.value.fDescription,
+      assigned_to: this.userId, column_id: this.columnId, created_at: new Date(), due_date: this.addCardForm.value.fDueDate},
+    ])
+  }
+
+  public async getValidUsers(){
+    await supabase.auth.getUser().then((response) => {
+      this.userId = response.data.user?.id
+      this.userMail = response.data.user?.email
+    })
+  }
+
+  ngOnInit(): void {
+    this.getValidUsers()
+    this.asignees.push(this.userMail)
+  }
+
+}
