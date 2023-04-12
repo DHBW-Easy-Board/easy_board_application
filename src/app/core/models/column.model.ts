154c36eb64bb0a3099e2b0b6c3af230ce56c661d
Jonny Vang <jonny-vang@web.de>
2023-04-13 00:47:06 +0200

    EB-57 added backend requests to load board data

---
+++ src/app/core/models/column.model.ts
@@ -0,0 +1,8 @@
+export interface Column {
+    id: number;
+    board_id: number;
+    name: string;
+    maximum_count: number;
+    position: number;
+    created_at: Date;
+}
--- src/app/features/kanban/pages/board/board.component.ts
+++ src/app/features/kanban/pages/board/board.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Column } from 'src/app/core/models/column.model';
+import { supabase } from 'src/env/supabase';
 
 @Component({
   selector: 'app-board',
@@ -6,5 +9,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent {
+    public title = 'Board';
+    public columns: Column[] = [];
 
+    // Inject activated route to access route parameters
+    constructor (private route: ActivatedRoute) { }
+
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            this.getBoard(params['id']);
+            this.getColums(params['id']);
+        });
+    }
+
+    /**
+     * Get data of the specfied board.
+     * ToDo Needs proper backend call.
+     * 
+     * @param boardId 
+     */
+    private async getBoard(boardId: number) {
+        await supabase.auth.getUser()
+            .then((response) => {
+                supabase.from('board_ov_vw')
+                    .select('*')
+                    .eq('owner_id', response.data.user?.id)
+                    .eq('board_id', boardId)
+                    .then((response) => {
+                        if (response.error)
+                            return;
+
+                        this.title = response.data[0]['board_name'];
+                        console.log(response);
+                    });
+            });
+    }
+
+    /**
+     * Get columns of the specified board.
+     * ToDo Needs proper backend call.
+     * 
+     * @param boardId 
+     */
+    private async getColums(boardId: number) {
+        await supabase.from('columns')
+            .select('*')
+            .eq('board_id', boardId)
+            .then((response) => {
+                if (response.error)
+                    return;
+                
+                this.columns = response.data as Column[];
+                console.log(response);
+            });
+    }
 }
