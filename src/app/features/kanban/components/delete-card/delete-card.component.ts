10acecbc0fdf4cff5deaa7320dd70d7e9e925143
CamelliaEnjoyer <BGAGpEi9rdxWrMd7BSuc>
2023-04-25 01:10:01 +0200

    EB-56_Delete_Card snackbar added (comment fixed)

--- src/app/features/kanban/components/delete-card/delete-card.component.ts
+++ src/app/features/kanban/components/delete-card/delete-card.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {supabase} from "../../../../../env/supabase";
 import {MatDialogRef} from "@angular/material/dialog";
 import {FormBuilder} from "@angular/forms";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-delete-card',
@@ -15,7 +16,7 @@ export class DeleteCardComponent {
    */
   public cardId = 0;
 
-  constructor(public dialogRef: MatDialogRef<DeleteCardComponent>) {  }
+  constructor(public dialogRef: MatDialogRef<DeleteCardComponent>, private snackBar: MatSnackBar) {  }
 
   /**
    * Dialog close function
@@ -25,11 +26,18 @@ export class DeleteCardComponent {
   }
 
   /**
-   * Deltes a card
+   * Delets a card by card id
    */
   public async deleteCard() {
-    await supabase.from('card').delete().eq('id', this.cardId)
-    this.dialogRef.close();
+    await supabase.from('card').delete().eq('id', this.cardId).then((response) => {
+      if(response.error){
+        console.log(response)
+        this.snackBar.open('deletion failed', 'ok')
+        this.dialogRef.close();
+      } else{
+        this.snackBar.open('deletion successful', 'ok')
+        this.dialogRef.close();
+      }
+    })
   }
-
 }
